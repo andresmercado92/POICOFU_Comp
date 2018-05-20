09692f2973f296faefbfa800d5039b63658698b8
Andres Mercado Niño <andres.mercadonino@gmail.com>
2018-05-20 15:56:58 -0500

    Creacion del proyecto en Django, creacion de la app Usuario y su modelo.

---
+++ POICOFU/apps/usuario/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Usuario(models.Model):
+	nombres= models.CharField(max_length= 50)
+	apellidos= models.CharField(max_length= 50)
+	edad= models.IntegerField()
+	ciudad_origen= models.CharField(max_length= 50)
+	pais= models.CharField(max_length= 50)
+	lenguaje_preferencia= models.CharField(max_length= 50)
